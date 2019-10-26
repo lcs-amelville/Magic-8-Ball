1fabbe9a06eb4a5a865a17004b1b7428c5096b11
aidan melville <amelville@lcs.on.ca>
2019-10-25 23:35:21 -0400

    Initial Commit

---
+++ Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Melville, Aidan on 2019-10-25.
+//  Copyright © 2019 Melville, Aidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
